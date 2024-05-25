6c7d19da5d8e09a6e8ab191b7e31fbf08dbb6a8c
Siddhartha-Kolla <siddharthansg@gmail.com>
2024-05-25 23:32:29 +0200

    Added the Round display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React, { useState, useEffect, useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Grid from '@mui/material/Grid';
 import Inter from '@fontsource/inter';
-import React, { useState, useEffect, useMemo } from 'react';
+import styles from './styles.module.css'; 
 
-const Table = ({team1,team2, onDragStart, onDrop, index, isLocked}) => {
+const Table = ({ team1, team2, onDragStart, onDrop, index, isLocked }) => {
   return (
     <div className='table'>
       <div 
@@ -39,58 +40,9 @@ const Table = ({team1,team2, onDragStart, onDrop, index, isLocked}) => {
   );
 }
 
-function PointTable() {
-    return (
-      <div className='points-table'>
-        <div className='point_table'>
-          <Grid container className='point_label'>
-            <Grid item xs={5}>Table 1</Grid>
-          </Grid>
-          <Grid container className='point_score'>
-            <Grid item xs={5} className='side'>1 | 2</Grid>
-            <Grid item xs={2} className='vs'>vs</Grid>
-            <Grid item xs={5} className='side'>3 | 4</Grid>
-          </Grid>
-          <Grid container className='point_label'>
-            <Grid item xs={5}>Table 1</Grid>
-          </Grid>
-          <Grid container className='point_score'>
-            <Grid item xs={5} className='side'>1 | 2</Grid>
-            <Grid item xs={2} className='vs'>vs</Grid>
-            <Grid item xs={5} className='side'>3 | 4</Grid>
-          </Grid>
-          <Grid container className='point_label'>
-            <Grid item xs={5}>Table 1</Grid>
-          </Grid>
-          <Grid container className='point_score'>
-            <Grid item xs={5} className='side'>1 | 2</Grid>
-            <Grid item xs={2} className='vs'>vs</Grid>
-            <Grid item xs={5} className='side'>3 | 4</Grid>
-          </Grid>
-          <Grid container className='point_label'>
-            <Grid item xs={5}>Table 1</Grid>
-          </Grid>
-          <Grid container className='point_score'>
-            <Grid item xs={5} className='side'>1 | 2</Grid>
-            <Grid item xs={2} className='vs'>vs</Grid>
-            <Grid item xs={5} className='side'>3 | 4</Grid>
-          </Grid>
-          <Grid container className='point_label'>
-            <Grid item xs={5}>Table 1</Grid>
-          </Grid>
-          <Grid container className='point_score'>
-            <Grid item xs={5} className='side'>1 | 2</Grid>
-            <Grid item xs={2} className='vs'>vs</Grid>
-            <Grid item xs={5} className='side'>3 | 4</Grid>
-          </Grid>
-        </div>
-      </div>
-    )
-  }
-
 function rotateRight(arr) {
   if (arr.length <= 1) {
-      return arr;
+    return arr;
   }
   
   const lastElement = arr.pop();
@@ -102,44 +54,60 @@ function rotateRight(arr) {
 function distributePlayers(players_num) {
   var rounds = [];
   var players_list = [];
-  for (let i=1;i <= players_num;i++) {
+  for (let i = 1; i <= players_num; i++) {
     players_list.push(i);
   }
 
-  for (let i=1;i < players_num;i++) {
+  for (let i = 1; i < players_num; i++) {
     var game = [];
-    for (let x=1;x<= Math.floor(players_num/2);x++) {
-      game.push([players_list[x-1],players_list.slice(x*(-1))[0]]);
+    for (let x = 1; x <= Math.floor(players_num / 2); x++) {
+      game.push([players_list[x - 1], players_list.slice(x * (-1))[0]]);
     }
     var turned_list = players_list.slice(1);
     turned_list = rotateRight(turned_list);
     var turned_list_final = [players_list[0]];
     turned_list_final = turned_list_final.concat(turned_list);
     players_list = turned_list_final;
-    rounds.push(game)
+    rounds.push(game);
   }
 
   return rounds;
 }
 
-function App() {
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
+function App() {
+  const [round, setRound] = useState(0);
+  const [play_round, setPlayRound] = useState(0);
   const [tables, setTables] = useState([]);
   const [draggedIndex, setDraggedIndex] = useState(null);
   const [draggedHalf, setDraggedHalf] = useState(null);
   const [isLocked, setIsLocked] = useState(false);
 
-  const addTable = (team1, team2) => {
-    const newTable = { id: tables.length + 1, team1, team2 };
-    setTables(prevTables => [...prevTables, newTable]);
-  };
+  const players = 24;
+  const player_board = useMemo(() => distributePlayers(players), [players]);
 
-  const updateTable = (id, newTeam1, newTeam2) => {
-    const updatedTables = tables.map(table =>
-      table.id === id ? { ...table, team1: newTeam1, team2: newTeam2 } : table
-    );
-    setTables(updatedTables);
-  };
+  const [playOrder, setPlayOrder] = useState(() => {
+    let order = [];
+    for (let i = 0; i < player_board[round].length / 2; i++) {
+      order.push([player_board[round][2 * i], player_board[round][2 * i + 1]]);
+    }
+    return order;
+  });
+
+  useEffect(() => {
+    const newTables = [];
+    for (let x = 0; x < players / 4; x++) {
+      newTables.push({ id: x + 1, team1: playOrder[x][0], team2: playOrder[x][1] });
+    }
+    setTables(newTables);
+  }, [playOrder]);
 
   const handleDragStart = (e, index, half) => {
     setDraggedIndex(index);
@@ -164,7 +132,7 @@ function App() {
     setTables(newTables);
 
     // Update play_order array
-    let newPlayOrder = [...play_order];
+    let newPlayOrder = [...playOrder];
     let temp = newPlayOrder[draggedIndex][draggedHalf];
     newPlayOrder[draggedIndex][draggedHalf] = newPlayOrder[index][half];
     newPlayOrder[index][half] = temp;
@@ -179,37 +147,41 @@ function App() {
     setIsLocked(prevIsLocked => !prevIsLocked);
   };
 
-  let players = 16;
-  let player_board = useMemo(() => distributePlayers(players), [players]);
-  player_board = player_board.sort((a, b) => 0.5 - Math.random());
-  
-  let [play_order, setPlayOrder] = useState(() => {
-    let round = 0;
-    let order = [];
-    for (let i = 0; i < player_board[round].length / 2; i++) {
-      order.push([player_board[round][2 * i], player_board[round][2 * i + 1]]);
-    }
-    return order;
-  });
+  const anotherRound = () => {
+    const newRound = (round + 1) % player_board.length;
+    setRound(newRound);
 
-  useEffect(() => {
-    for (let x = 0; x < players / 4; x++) {
-      addTable(play_order[x][0], play_order[x][1]);
+    let newOrder = [];
+    for (let i = 0; i < player_board[newRound].length / 2; i++) {
+      newOrder.push([player_board[newRound][2 * i], player_board[newRound][2 * i + 1]]);
     }
-  }, []);
+    setPlayOrder(newOrder);
+  };
 
-  function end_game() {
-    console.log(play_order);
+  const handleRandomize = () => {
+    const shuffledOrder = shuffle([...playOrder]);
+    setPlayOrder(shuffledOrder);
+  };
+
+  const appendRound = () => {
+    if  (!(play_round>=players-2)) {
+      let newPlayRound = play_round + 1;
+      setPlayRound(newPlayRound);
+      player_board.splice(round,1);
+      anotherRound();
+    }
   }
 
   return (
     <div className="App">
-      <header className="App-header flex bg-green-600">
+      <header className="App-header flex">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Round {play_round+1}</p>
+        <button onClick={appendRound} className='button next-button' style={{width: "5vw"}}>{">"}</button>
       </header>
       <body className='App-body bg-black'>
         <Grid container>
-          <Grid item xs={8} className="bg-green-600 tables">
+          <Grid item xs={8} className="tables">
             {tables.map((table, index) => (
               <Table 
                 key={table.id} 
@@ -223,12 +195,9 @@ function App() {
             ))}
           </Grid>
           <Grid item xs={4} className='bg-green-600 control-panel'>
-            <button className='button'>Generate</button>
-            <button className='button'>End</button>
+            <button className='button' onClick={anotherRound}>Another round</button>
+            <button className='button' onClick={handleRandomize} disabled={isLocked}>Randomize</button>
             <button className='button' onClick={toggleLock}>{isLocked ? 'Unlock' : 'Lock'}</button>
-            <button className='end-button' onClick={end_game}>End Game</button>
-            <PointTable/>
-            <div className='points'></div>
           </Grid>
         </Grid>
       </body>
